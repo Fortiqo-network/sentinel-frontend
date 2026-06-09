a1e7b22affa7ee88883552ce114e000fa034c554
Shivani Asati <asati15shivani@gmail.com>
2026-06-09 12:39:28 +0000

    fix: remove duplicate marketplace root page that conflicts with app/page.tsx

--- src/app/(marketplace)/_client-boundary.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import * as React from "react";
-
-export function ClientBoundary({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
--- src/components/marketplace/AgentGrid.tsx
+++ src/components/marketplace/AgentGrid.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import * as React from "react";
 import { AgentCard } from "./AgentCard";
 import { AgentCardSkeleton } from "@/components/ui/skeleton";
